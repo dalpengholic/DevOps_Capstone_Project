7941b6c0cf2ffddbfa02dc7990a436c2a65cd217
Woojae Gabriel Lim <dalpengholic@gmail.com>
2020-03-31 23:42:51 +0300

    edit Jenkinsfile

--- Infra/create.sh
+++ Infra/create.sh
@@ -1,4 +1,4 @@
-aws cloudformation create-stack \
+/home/ubuntu/.local/bin/aws cloudformation create-stack \
 --stack-name $1 \
 --template-body file://$2 \
 --region us-west-2 \
--- Jenkinsfile
+++ Jenkinsfile
@@ -64,6 +64,7 @@ pipeline{
         }
       }
     }
+    
     stage('Create EKS Node Stack'){
       steps{
         dir('Infra'){
